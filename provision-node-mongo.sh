888aa3f5ad4c455bcd4a4a58d6fe8717491056f5
Dmitry Kalinin <kal1sha.ua@gmail.com>
2016-08-09 23:27:03 +0300

    Update provision-node-mongo.sh

--- provision-node-mongo.sh
+++ provision-node-mongo.sh
@@ -3,12 +3,12 @@ sudo apt-get -y update
 sudo apt-get -y install python-software-properties
 locale-gen UTF-8
 
-cd /opt && \
-wget http://nodejs.org/dist/v0.10.31/node-v0.10.31-linux-x64.tar.gz && \
-tar zxf node-v0.10.31-linux-x64.tar.gz && \
-ln -s /opt/node-v0.10.31-linux-x64/bin/node /usr/local/bin/node && \
-ln -s /opt/node-v0.10.31-linux-x64/bin/node-waf /usr/local/bin/node-waf && \
-ln -s /opt/node-v0.10.31-linux-x64/bin/npm /usr/local/bin/npm
+sudo apt-get update
+sudo apt-get install -y curl
+sudo apt-get install -y npm
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install -y nodejs
+sudo apt-get install -y build-essential libssl-dev
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 && \
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list && \
@@ -22,5 +22,5 @@ echo "mongodb-org-tools hold" | dpkg --set-selections && \
 sed -i '/bind_ip = 127.0.0.1/,/bind_ip\ =\ 127\.0\.0\.1/s/^/#/' /etc/mongod.conf && \
 service mongod restart
 
-curl https://install.meteor.com | sudo sh
+curl https://install.meteor.com | /bin/sh
 sudo npm install -g meteorite
